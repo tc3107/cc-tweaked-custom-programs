b0ffdcad1f9a1e6d760c5da7c1984aec2b669dad
tc3107 <tc3107@proton.me>
2025-05-12 17:43:30 +0300

    Create bridge.lua

---
+++ bridge.lua
@@ -0,0 +1,9 @@
+rednet.open('top')
+rednet.open('bottom')
+rednet.host('lookup', 'bridge')
+
+while true do
+  local id, msg, proto = rednet.receive()
+  rednet.broadcast(msg, proto)
+  print('Broadcasted: ', proto)
+end
