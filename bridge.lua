a45687f6ab8dde1708478b60810fc7e68a57069e
tc3107 <tc3107@proton.me>
2025-05-12 17:50:42 +0300

    Update bridge.lua

--- bridge.lua
+++ bridge.lua
@@ -3,7 +3,7 @@ rednet.open('bottom')
 rednet.host('lookup', 'bridge')
 
 while true do
-  local id, msg, proto = rednet.receive()
-  rednet.broadcast(msg, proto)
-  print('Broadcasted: ', proto)
+    local id, msg, proto = rednet.receive()
+    rednet.broadcast(msg, proto)
+    print('Broadcasted: ', proto)
 end
