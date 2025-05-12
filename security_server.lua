5e026a50d06c7a4510c3280441d6fdcd0a907a30
tc3107 <tc3107@proton.me>
2025-05-12 20:06:35 +0300

    Update security_server.lua

--- security_server.lua
+++ security_server.lua
@@ -1,5 +1,29 @@
-local relay = peripherals.wrap('redstone_relay_2')
+local relay = peripheral.wrap('redstone_relay_2')
 rednet.open('left')
 rednet.host('auth', 'airlock_control')
 
-if 
+local authDict = {
+    main_airlock = true
+}
+
+local function updateDict()
+    authDict["main_airlock"] = relay.getInput('right')
+end
+
+while true do
+    local id, msg, proto = rednet.receive()
+    if proto == 'auth' then
+        print("Request received from", id, "for:", msg)
+        updateDict()
+        
+        if authDict[msg] == true then
+            rednet.send(id, true, 'auth')
+            print("Approved.")
+        elseif authDict[msg] == false then
+            rednet.send(id, false, 'auth')
+            print("Denied.")
+        else
+            print("Not found.")
+        end
+    end
+end
