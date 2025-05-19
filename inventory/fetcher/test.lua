e842a023406dbcb15ab7efa49be1ad2f92d65b9e
tc3107 <tc3107@proton.me>
2025-05-19 20:25:08 +0300

    Delete inventory/fetcher/test.lua

--- inventory/fetcher/test.lua
+++
@@ -1,8 +0,0 @@
-local side = "bottom"
-local attachedPeripheral = peripheral.wrap(side)
-for _, name in ipairs(peripheral.getNames()) do
-  if peripheral.wrap(name) == attachedPeripheral then
-    print("Network name:", name)
-    break
-  end
-end
