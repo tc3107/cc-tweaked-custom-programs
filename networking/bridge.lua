b114db194e0cf1d0fab30180223d3780253bf159
tc3107 <tc3107@proton.me>
2025-05-20 19:19:47 +0300

    Delete networking/bridge.lua

--- networking/bridge.lua
+++
@@ -1,9 +0,0 @@
-rednet.open('top')
-rednet.open('bottom')
-rednet.host('lookup', 'bridge')
-
-while true do
-    local id, msg, proto = rednet.receive()
-    rednet.broadcast(msg, proto)
-    print('Broadcasted: ', proto)
-end
